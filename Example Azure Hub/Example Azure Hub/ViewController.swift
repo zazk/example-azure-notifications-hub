8b581e8878ee5ff302c46ac75677ed0769da84a6
Enrique Juan de Dios <juandedioz@gmail.com>
2016-05-02 23:12:15 -0500

    Add Project Empty

---
+++ Example Azure Hub/Example Azure Hub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example Azure Hub
+//
+//  Created by user on 5/2/16.
+//  Copyright © 2016 cubix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
